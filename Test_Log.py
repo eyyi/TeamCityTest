240e34b833aed05395fe9c77690247830f1fa358
eyyi <eyyi@zhaw.ch>
2015-09-07 15:12:59 +0200

    firt commit

--- Test_Log.py
+++ Test_Log.py
@@ -11,6 +11,12 @@ def print_current_date():
 def main():
     print_current_date()
 
+def func(x):
+    return x + 1
+
+def test_answer():
+    assert func(3) == 4
 
 if __name__ == '__main__':
     main()
+    test_answer()
