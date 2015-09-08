78536bd24d86174d06b6781d787785f9638db02b
eyyi <eyyi@zhaw.ch>
2015-09-08 09:56:26 +0200

    test teamcity-messages

--- Test_Log.py
+++ Test_Log.py
@@ -2,6 +2,7 @@ __author__ = 'gabrieleyyi'
 
 from datetime import date
 
+import Unit_Test as un
 
 def print_current_date():
     today = date.today()
@@ -12,6 +13,7 @@ def main():
     print_current_date()
 
 def func(x):
+    ##teamcity[<messageName> timestamp='timestamp' ...]
     return x + 1
 
 def test_answer():
@@ -20,3 +22,9 @@ def test_answer():
 if __name__ == '__main__':
     main()
     test_answer()
+
+    if un.is_running_under_teamcity():
+        runner = un.TeamcityTestRunner()
+    else:
+        runner = un.unittest.TextTestRunner()
+    un.unittest.main(testRunner=runner)
